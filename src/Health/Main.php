973e6b5f2bbe8197288d77d705210dbd684ec970
Hariz-dev-0 <68668776+Hariz-dev-0@users.noreply.github.com>
2020-07-24 16:53:35 +0700

    Add Config And Bow Shoot Sound

--- src/Health/Main.php
+++ src/Health/Main.php
@@ -4,18 +4,22 @@ namespace Health;
 
 use pocketmine\Player;
 use pocketmine\Server;
-
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\event\Listener;
-
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\utils\Config;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 
 class Main extends PluginBase implements Listener{
 
    public function onEnable(){
         $this->getLogger()->warning("§aPlugin Has Work");
         $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        
+        @mkdir($this->getDataFolder());
+	$this->saveResource("config.yml");
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     }
     public function onLoad(){
         $this->getLogger()->warning("Plugin Has Loading");
@@ -30,22 +34,30 @@ class Main extends PluginBase implements Listener{
                 $dmg = $ev->getDamager();
                 $entity = $ev->getEntity();
                 if($dmg instanceof Player and $entity instanceof Player){
+                	$level = $dmg->getLevel();
                     $health = $entity->getHealth();
                     $name = $entity->getName();
+                    $playernamehit = $dmg->getName();
                     $maxHealth = $entity->getMaxHealth();
-                     $dmg->sendMessage("§eHealth §a{$name} §4[§e{$health}/{$maxHealth}§4]§c♥");
+                    
+                     $dmg->sendMessage(str_replace("{player-name}", $name, str_replace("{line}", "\n", str_replace("{health}", $health, str_replace("{maxhealth}", $maxHealth, $this->cfg->get("Player-Hit-Health"))))));
+                     $level->broadcastLevelSoundEvent($dmg->getPosition(), LevelSoundEventPacket::SOUND_ATTACK_STRONG);
                 }
             break;
             case EntityDamageEvent::CAUSE_PROJECTILE:
                 $dmg = $ev->getDamager();
                 $entity = $ev->getEntity();
                 if($dmg instanceof Player and $entity instanceof Player){
+                	$level = $dmg->getLevel();
                     $health = $entity->getHealth();
                     $name = $entity->getName();
+                    $playernamehit = $dmg->getName();
                     $maxHealth = $entity->getMaxHealth();
-                     $dmg->sendMessage("§eHealth §a{$name} §4[§e{$health}/{$maxHealth}§4]§c♥");
-                 }
-             break;
+                    
+                     $dmg->sendMessage(str_replace("{player-name}", $name, str_replace("{line}", "\n", str_replace("{health}", $health, str_replace("{maxhealth}", $maxHealth, $this->cfg->get("Bow-Hit-Health"))))));
+                     $level->broadcastLevelEvent($dmg->getPosition(), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
+                }
+            break;
         }
     }
 }
